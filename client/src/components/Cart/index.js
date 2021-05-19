6429ea49302c6ae6d8c2e17248258decf0ec6f44
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-19 16:28:31 -0400

    implemented stripe and got it working with the cart and redirecting back to the home page after payment was submitted

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,15 +5,30 @@ import "./style.css";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useLazyQuery } from "@apollo/react-hooks";
+// =================== stripe =============================== //
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
   const [state, dispatch] = useStoreContext();
+  // ================ useEffect specifically for stripe START ====================== //
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+  // ================ useEffect specifically for stripe END ====================== //
   useEffect(() => {
     async function getCart() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    };
-  
+    }
+
     if (!state.cart.length) {
       getCart();
     }
@@ -29,6 +44,18 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds },
+    });
   }
 
   if (!state.cartOpen) {
@@ -56,7 +83,7 @@ const Cart = () => {
             <strong>Total: ${calculateTotal()}</strong>
 
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
